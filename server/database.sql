714a3ad968b429e3959ba6fc30b6b524b6eb6947
Nicholas Locantore <nlocantore30@gmail.com>
2021-05-21 15:25:24 -0400

    Chagned database file

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,15 @@
--- CREATE DATABASE test;
+CREATE DATABASE test;
 
--- CREATE TABLE test(
---     test_id SERIAL PRIMARY KEY,
---     description VARCHAR(255)
--- );
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    username VARCHAR(25),
+    password VARCHAR(25),
+    type INTEGER   (0 is Employer, 1 is Employee)
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    description VARCHAR(100)
+);
