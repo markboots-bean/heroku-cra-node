a770d955f590a61c38be3e2f7738946a06ecd787
unknown <mb4245@drexel.edu>
2021-05-27 15:53:24 -0400

    update view

--- react-ui/src/components/TaskView.js
+++ react-ui/src/components/TaskView.js
@@ -1,18 +1,20 @@
 import React, { Fragment, useState } from 'react';
 
-const TaskView = () => {
+const TaskEmployer = () => {
     return (
         <Fragment>
             <h1 className="text-center mt-2">Task Viewer</h1>
-            <h2 className="text-center pt-2">Task Viewer</h2>
+            <h2 className="text-center pt-2">Current Tasks</h2>
             <table className="pt-1 px-1">
                 <thead>
                 <tr>
                     <th px-1>Task</th>
-                    <th px-3>Task Description</th>
-                    <th px-1>Complete</th>
-                    </tr>
+                    <th px-4>Task Description</th>
+                    <th px-0>Complete</th>
+                </tr>   
                 </thead>
+                <tbody id="employee-task-table"></tbody>
+                <button className="btn btn-success">Mark as Complete</button>
             </table>
         </Fragment>
     )
