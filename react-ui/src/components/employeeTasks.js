247b65eb707a84949aa301f9c93a46e644b9f434
unknown <mb4245@drexel.edu>
2021-05-31 15:19:44 -0400

    pulling

--- react-ui/src/components/employeeTasks.js
+++ react-ui/src/components/employeeTasks.js
@@ -1,23 +1,24 @@
 import React, { Fragment, useState } from 'react';
 
-const TaskEmployer = () => {
+const employeeTasks = () => {
     return (
         <Fragment>
             <h1 className="text-center mt-2">Task Viewer</h1>
             <h2 className="text-center pt-2">Current Tasks</h2>
-            <table className="pt-1 px-1">
+            <table class="table mt-5 text-center">
                 <thead>
-                <tr>
-                    <th px-1>Task</th>
-                    <th px-4>Task Description</th>
-                    <th px-0>Complete</th>
-                </tr>   
+                    <tr>
+                        <th>Task</th>
+                        <th>Task Description</th>
+                        <th>Complete</th>
+                    </tr>   
                 </thead>
                 <tbody id="employee-task-table"></tbody>
-                <button className="btn btn-success">Mark as Complete</button>
             </table>
         </Fragment>
     )
     
 }
+
+export default employeeTasks;
     
